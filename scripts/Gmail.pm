3a3633ada73291f8c341ff960c3e7e7a6e31b5e8
James Powell <jamespo@gmail.com>
2013-03-28 21:38:05 +0000

    cleaned up POD, method names

--- scripts/Gmail.pm
+++ scripts/Gmail.pm
@@ -17,7 +17,6 @@ sub get_labels {
 	final => sub { 
 	    my @labels = Text::ParseWords::parse_line(' ', 0, $label);
 	    return \@labels },
-#        final => sub { pop @lines; wantarray ? @lines : Net::IMAP::Simple::_message->new(\@lines) },
         process => sub {
             if ( $_[0] =~ /^\* \d+ FETCH \(X-GM-LABELS \((.+)\)\)\s*$/ ) {
                 $label = $1;
@@ -53,8 +52,21 @@ Net::IMAP::Simple::Gmail - Gmail specific support for Net::IMAP::Simple
 
 =head1 DESCRIPTION
 
-This module is a subclass of L<Net::IMAP::SImple|Net::IMAP::Simple:SSL> that
-includes SSL support. The interface is identical.
+This module is a subclass of L<Net::IMAP::Simple::SSL|Net::IMAP::Simple::SSL> that
+includes specific support for Gmail IMAP Extensions. Besides the gmail specific
+methods the interface is identical.
+
+=head1 METHODS
+
+=over 4
+
+=item get_labels
+
+$imap->get_labels($msgid);
+
+Returns an arrayref of all labels on the message.
+
+=back
 
 =head1 SEE ALSO
 
