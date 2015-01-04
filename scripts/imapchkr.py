7ac762e74892d4eddb37a3d164735008440dcc86
James Powell <jamespo@gmail.com>
2015-01-04 20:43:21 +0000

    handles failed logins

--- scripts/imapchkr.py
+++ scripts/imapchkr.py
@@ -10,14 +10,22 @@
 
 import os
 import imaplib
-#from optparse import OptionParser
+from optparse import OptionParser
 import ConfigParser
 import Queue
 import threading
 
-#def getopts():
-#    parser = OptionParser()
-#    parser.add_option("-s", help="search string", action="store", dest="txt")
+colmap = {      # shell escape codes
+    'NORM'      : '\033[0m',
+    'HILIT'     : '\033[37;1m',
+}
+
+def getopts():
+    parser = OptionParser()
+    parser.add_option("-b", help="b/w output", action="store_false",
+                      dest="color", default = True)
+    (options, args) = parser.parse_args()
+    return options
 
 def readconf():
     config = ConfigParser.ConfigParser()
@@ -26,24 +34,35 @@ def readconf():
 
 def checknew(q, account, user, pw, server, folder="INBOX"):
     '''returns tuple of account, unread, total # of messages'''
-    mail = imaplib.IMAP4_SSL(server)
-    mail.login(user, pw)
-    mail.list()
-    (allretcode, allmessages_str) = mail.select(folder) # connect to inbox.
-    (unretcode, unmessages) = mail.search(None, '(UNSEEN)')
-    if (unretcode, allretcode) == ('OK', 'OK'):
-        allmessages_num = int(allmessages_str[0])
-        if unmessages[0] == '':
-            q.put((account, 0, allmessages_num))
-        else:
-            q.put((account, len(unmessages[0].split(' ')), allmessages_num))
-    else:
-        q.put((account, None, None))
+    try: 
+        mail = imaplib.IMAP4_SSL(server)
+        mail.login(user, pw)
+        mail.list()
+        (allretcode, allmessages_str) = mail.select(folder) # connect to inbox.
+        (unretcode, unmessages) = mail.search(None, '(UNSEEN)')
+        if (unretcode, allretcode) == ('OK', 'OK'):
+            allmessages_num = int(allmessages_str[0])
+            if unmessages[0] == '':
+                q.put((account, 0, allmessages_num))
+            else:
+                q.put((account, len(unmessages[0].split(' ')), allmessages_num))
+            return
+    except:
+        pass
+    q.put((account, None, None))
 
-def format_msgcnt(server, new_msg, all_msg):
-    return '[%s: %d/%d]' % (server, new_msg, all_msg)
+def format_msgcnt(color, server, new_msg, all_msg):
+    if None in (new_msg, all_msg):
+        return '[%s: unknown]' % server
+    elif color and new_msg > 0:
+        return '[%s: %s%d/%d%s]' % (server, colmap['HILIT'],
+                                new_msg, all_msg, colmap['NORM'])
+    else:
+        return '[%s: %d/%d]' % (server, new_msg, all_msg)
+
 
 def main():
+    cmd_options = getopts()
     config = readconf()
     accounts = config.sections()
     q = Queue.Queue(len(accounts))
@@ -57,7 +76,7 @@ def main():
     counts = []
     for account in accounts:
         msg_results = q.get()
-        counts.append(format_msgcnt(*msg_results))
+        counts.append(format_msgcnt(cmd_options.color, *msg_results))
     print ' '.join(counts)
 
 if __name__ == '__main__':
