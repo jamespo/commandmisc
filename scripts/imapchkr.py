84abd5e6004fea6d63c77c47f40ca3dc9aed6b56
James Powell <jamespo@gmail.com>
2016-03-10 11:11:26 +0000

    clean from address, calc width of account field

--- scripts/imapchkr.py
+++ scripts/imapchkr.py
@@ -12,17 +12,16 @@ import os
 import imaplib
 import email
 from email.header import decode_header
+from email.utils import parseaddr
 from optparse import OptionParser
 from collections import namedtuple
 import ConfigParser
 import Queue
 import threading
 
-colmap = {      # shell escape codes
-    'NORM'      : '\033[0m',
-    'HILIT'     : '\033[37;1m',
-}
-
+# tuple of shell colour codes
+colmap = namedtuple('ColMap', ['norm', 'white', 'blue', 'yellow', 'green'])
+colm = colmap('\033[0m', '\033[37;1m', '\033[36;1m', '\033[33;1m', '\033[32;1m')
 
 def getopts():
     '''returns OptionParser.options for CL switches'''
@@ -53,24 +52,7 @@ def checknew(q, account, user, pw, server, get_summaries=False, folder="INBOX"):
         mail.login(user, pw)
         mail.list()
         # select inbox
-        (allretcode, allmessages_str) = mail.select(folder, readonly=True)
-        (unretcode, unmessages) = mail.search(None, '(UNSEEN)')
-        if (unretcode, allretcode) == ('OK', 'OK'):
-            allmessages_num = int(allmessages_str[0])
-            if unmessages[0] == '':
-                # no new mails found
-                mailinfo = MInfo(account, 0, allmessages_num, [])
-            else:
-                # new mails found
-                unmessages_arr = unmessages[0].split(' ')
-                if get_summaries:
-                    msgs = get_mails(mail, unmessages_arr)
-                else:
-                    msgs = []
-                mailinfo = MInfo(account, len(unmessages_arr),
-                                 allmessages_num, msgs)
-        else:
-            raise imaplib.IMAP4.error()
+        mailinfo = count_mails(mail, MInfo, folder, account, get_summaries)
     except:
         mailinfo = MInfo(account, None, None)
     finally:
@@ -79,7 +61,30 @@ def checknew(q, account, user, pw, server, get_summaries=False, folder="INBOX"):
         mail.logout()
     q.put(mailinfo)
 
-    
+
+def count_mails(mail, MInfo, folder, account, get_summaries):
+    '''count the mails & return MailInfo tuple'''
+    (allretcode, allmessages_str) = mail.select(folder, readonly=True)
+    (unretcode, unmessages) = mail.search(None, '(UNSEEN)')
+    if (unretcode, allretcode) == ('OK', 'OK'):
+        allmessages_num = int(allmessages_str[0])
+        if unmessages[0] == '':
+            # no new mails found
+            mailinfo = MInfo(account, 0, allmessages_num, [])
+        else:
+            # new mails found
+            unmessages_arr = unmessages[0].split(' ')
+            if get_summaries:
+                msgs = get_mails(mail, unmessages_arr)
+            else:
+                msgs = []
+            mailinfo = MInfo(account, len(unmessages_arr),
+                             allmessages_num, msgs)
+    else:
+        raise imaplib.IMAP4.error()
+    return mailinfo
+
+
 def get_mails(mail, msg_ids):
     '''return mail summaries for given msg_ids'''
     msgs = []
@@ -88,43 +93,54 @@ def get_mails(mail, msg_ids):
         for num in msg_ids:
             typ, data = mail.fetch(num, '(RFC822)')
             msg = email.message_from_string(data[0][1])
-            subj = msg['Subject']
-            # decode subject if in unicode format
-            # TODO: test for UTF-8 better than below
-            if 'UTF-8' in subj:
-                decd_subj = decode_header(subj)
-                # TODO: what is default_charset?
-                subj = ''.join([ unicode(t[0], t[1] or default_charset) for t in decd_subj ])
-            email_summ = EmailSummary(int(num), msg['From'], subj, None)
+            subj = clean_subject(msg['Subject'])
+            fromad = clean_address(msg['From'])
+            email_summ = EmailSummary(int(num), fromad, subj, None)
             msgs.append(email_summ)
     except:
         # if any errors just don't list mails
         msgs = []
     return msgs
 
+def clean_address(email_addr):
+    '''return clean from address'''
+    clean_addr = parseaddr(email_addr)
+    return clean_addr[0] or clean_addr[1]
+
+def clean_subject(subj):
+    '''decode subject if in unicode format'''
+    # TODO: test for UTF-8 better than below
+    if 'UTF-8' in subj:
+        decd_subj = decode_header(subj)
+        # TODO: what is default_charset?
+        subj = ''.join([ unicode(t[0], t[1] or default_charset) for t in decd_subj ])
+    # remove newlines
+    subj = subj.replace('\r', '')
+    subj = subj.replace('\n', '')   
+    return subj
+
 def format_mailsummaries(mailinfos):
     '''takes list of MailInfo tuple & returns formatted string of mails'''
-    summstr = ''
-    # TODO: build up array & join it instead
+    ac_max_len = max((len(mailinfo.account) for mailinfo in mailinfos))
+    summaries = []
     for mailinfo in mailinfos:
         for summ in mailinfo.msgs:
-            summstr +=  "[%s] [%0.4d] %.25s  %.40s\n" % (mailinfo.account, summ.num, \
-                                                    summ.fromad, summ.subject)
-    return summstr
+            summaries.append("{acct:{ac_max_len}} [{summ.num:04d}] {summ.fromad:25} {summ.subject:40}".format(acct=mailinfo.account, ac_max_len=ac_max_len, summ=summ))
+    return "\n".join(summaries)
 
-def format_msgcnt(options, accounts):
+def format_msgcnt(options, mailtotals):
     '''returns string with account overview (read/unread)'''
     output = ''
-    for acct in accounts:
+    for mailtotal in mailtotals:
         if options.short:
-            output += "%s:%s " % (acct.account[0], acct.unread)
-        elif None in (acct.unread, acct.total):
-            output += '[%s: unknown] ' % acct.account
-        elif options.color and acct.unread > 0:
-            output += '[%s: %s%d/%d%s] ' % (acct.account, colmap['HILIT'],
-                                acct.unread, acct.total, colmap['NORM'])
+            output += "%s:%s " % (mailtotal.account[0], mailtotal.unread)
+        elif None in (mailtotal.unread, mailtotal.total):
+            output += '[%s: unknown] ' % mailtotal.account
+        elif options.color and mailtotal.unread > 0:
+            output += '[%s: %s%d/%d%s] ' % (mailtotal.account, colm.white,
+                                            mailtotal.unread, mailtotal.total, colm.norm)
         else:
-            output += '[%s: %d/%d] ' % (acct.account, acct.unread, acct.total)
+            output += '[%s: %d/%d] ' % (mailtotal.account, mailtotal.unread, mailtotal.total)
     output = output.rstrip()
     return output
 
@@ -143,14 +159,14 @@ def main():
                              args = (q, account, user, pw, server,
                                      cmd_options.listmail))
         t.start()
-    counts = []
+    mailtotals = []
     for account in accounts:
         msg_results = q.get()
-        counts.append(msg_results)
-    print format_msgcnt(cmd_options, counts)
+        mailtotals.append(msg_results)
+    print format_msgcnt(cmd_options, mailtotals)
     # display email summaries if chosen and any new mails
-    if cmd_options.listmail and any([msg.unread > 0 for msg in counts]):
-        print format_mailsummaries(counts).rstrip()
+    if cmd_options.listmail and any([msg.unread > 0 for msg in mailtotals]):
+        print format_mailsummaries(mailtotals).rstrip()
 
 if __name__ == '__main__':
     main()
