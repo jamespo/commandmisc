0627d85597a6cea13b1eead675f4fe10799c71fa
James Powell <jamespo@gmail.com>
2016-07-04 12:04:32 +0100

    initial

---
+++ scripts/getquote.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# getquote.py - get quote from google
+# USAGE: getquote.py LSE:BARC
+
+from __future__ import print_function
+import requests
+import json
+import sys
+
+def main(ticker):
+    '''get the quote from google & output it'''
+    r = requests.get('http://www.google.com/finance/info?q=%s' % ticker)
+    assert r.status_code == 200
+    # hack to remove leading comment
+    quote_json = r.content.lstrip().lstrip('/')
+    results = json.loads(quote_json)[0]
+    print('%s: %s (%s%%)' % (results['t'], results['l'], results['cp_fix']))
+    
+if __name__ == '__main__':
+    try:
+        main(sys.argv[1])
+    except IndexError:
+        print('ERROR: must supply ticker code')
+        
