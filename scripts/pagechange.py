5fc364b98082bcf32042eeda369fb05e1acd648c
James Powell <jamespo@gmail.com>
2016-11-08 12:02:34 +0000

    some exception catching. wipe ref between reqs

--- scripts/pagechange.py
+++ scripts/pagechange.py
@@ -13,9 +13,11 @@ import re
 import cPickle as pickle
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from optparse import OptionParser
 from pyvirtualdisplay import Display
 
+# default
 SAVEDIR = os.path.join(os.path.expanduser('~'), '.pagechange')
 
 
@@ -31,7 +33,7 @@ class Page(object):
         self.title = ''
 
     def save(self):
-        '''Pickle Page object'''
+        '''Pickle Page object (self)'''
         if self.filename is None:
             # create alphanumeric filename from URL
             self.filename = re.sub('(^https?://|[\W_]+)', '', self.url) + '.pk'
@@ -41,16 +43,20 @@ class Page(object):
             pickle.dump(self, picklefile)
 
     def __str__(self):
-        # return self.match.encode('utf-8')
+        '''printable representation of self'''
         return "Title: %s\nOld: (%s) New: (%s)\nURL: %s" \
             % (self.title.encode('utf-8'), self.oldmatch.encode('utf-8'),
                self.match.encode('utf-8'), self.url)
 
     def load(self):
+        '''load pickled page object'''
         with open(os.path.join(self.savedir, self.filename), 'rb') as pfile:
+            savedir = self.savedir
             a = pickle.load(pfile)
             # copy pickle object into self
             self.__dict__ = a.__dict__.copy()
+            # restore savedir
+            self.savedir = savedir
 
             
 class PageChange(object):
@@ -68,11 +74,16 @@ class PageChange(object):
         
     def check(self, page):
         '''return content in page matching xpath'''
+        # insert dummy about to wipe referer
+        self.driver.get('about:')
         self.driver.get(page.url)
         self.driver.implicitly_wait(self.waittime)
         # assert "Python" in driver.title
         self.xpmatch = self.driver.find_element(By.XPATH, page.xpath)
+        # cookie cleanup - make optional?
+        self.driver.delete_all_cookies()
         return self.xpmatch.text
+
 
     def update_page(self, page):
         '''updates page object'''
@@ -94,12 +105,13 @@ def get_options():
     parser.add_option("-b", help="browser", dest="browser",
                       default="firefox")
     parser.add_option("-d", help="savedir", dest="savedir",
-                      default=os.path.join(os.path.expanduser('~'), '.pagechange'))
+                      default=SAVEDIR)
     (options, args) = parser.parse_args()
     return (options, args)
 
 
 def get_all_checks(savedir):
+    '''loads all checks in savedir and returns list'''
     checkfiles = [cf for cf in os.listdir(savedir) if cf[-3:] == '.pk']
     checks = []
     for cf in checkfiles:
@@ -110,38 +122,49 @@ def get_all_checks(savedir):
                   
 
 def list_pages(savedir):
+    '''displays all checks in savedir'''
     for check in get_all_checks(savedir):
         print check
         print ''
 
 
 def check_pages(options):
+    '''run all checks & display changed'''
     pc = PageChange(browser=options.browser)
     for check in get_all_checks(options.savedir):
-        pc.check(check)
-        if pc.update_page(check):
-            # changed
-            print check
+        try:
+            pc.check(check)
+            if pc.update_page(check):
+                # changed
+                print check
+            elif os.getenv('DEBUG'):
+                print 'No change for %s' % check.title
+        except NoSuchElementException as e:
+            # print '%s failed - error: %s' % (check.title, str(e))
+            print '%s failed - error: %s' % (check.title, e)
     pc.driver.close()
 
 def add_page(options):
+    '''add a check'''
     p = Page(options.savedir, options.url, options.xpath)
     pc = PageChange(browser=options.browser)
     try:
         pc.check(p)
-    except:
-        pass
-    pc.update_page(p)
-    print p
+        pc.update_page(p)
+        print p
+    except NoSuchElementException:
+        print 'Cannot find match in page - not saving'
     pc.driver.close()
 
 def virt_display(function, options):
+    '''launch virtual display (for selenium) and run function'''
     display = Display(visible=0, size=(800, 600))
     display.start()
     function(options)
     display.stop()
 
 def main():
+    '''check args and run appropriate function'''
     (options, args) = get_options()
     if options.mode == 'add':
         if None in (options.url, options.xpath):
